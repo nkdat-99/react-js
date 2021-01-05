0ecbf71b2cd3331a1e181a2a255d5a778b3f1b92
datmaster <66270287+nkdat-99@users.noreply.github.com>
2021-01-05 09:20:20 +0700

    Update

---
+++ src/component/TodoList/todo-item.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './todo-item.css';
+import classNames from 'classnames';
+import { DeleteTwoTone } from '@ant-design/icons';
+import { Tag } from 'antd';
+
+export const TodoItem = (props: any) => {
+    return (
+        <div className="todo-item">
+            <div className="d-flex item-left"> 
+                <div onClick={props.isActiveItem} className={classNames("checkbox", {'checkbox-done' : !props.item.isActive})}></div>
+                <div className={classNames("title", {'title-done' : !props.item.isActive})}>{props.item.title}</div>
+                <Tag className="item-type" color="#2db7f5">{props.item.type}</Tag>     
+            </div>
+            <div className="d-flex item-right">
+                <Tag className="item-datetime" color="#f50">{props.item.datetime}</Tag>    
+                <DeleteTwoTone className="icon-del" twoToneColor="#ff0000" onClick={props.delItem}/>
+            </div> 
+        </div>
+    )
+}
--- src/component/TodoList/todo-list.tsx
+++ src/component/TodoList/todo-list.tsx
@@ -1,56 +1,129 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from "react";
 // import axios from 'axios';
-import { List, Typography, Divider, Button } from 'antd';
-import 'antd/dist/antd.css';
-import './todo-list.css';
-import Search from 'antd/lib/input/Search';
+import { List, Divider, Button } from "antd";
+import "antd/dist/antd.css";
+import "./todo-list.css";
+import Search from "antd/lib/input/Search";
+import { TodoItem } from "./todo-item";
 
 export const TodoList = () => {
-    // const [isLoaded, setIsLoaded] = useState(false);
-    // const [listUser, setItems] = useState([]);
-    const data = [
-        'Racing car sprays burning fuel into crowd.',
-        'Japanese princess to wed commoner.',
-        'Australian walks 100km after outback crash.',
-        'Man charged over missing wedding girl.',
-        'Los Angeles battles huge wildfires.',
-    ];
+    const todoItems = [
+        {
+            title: 'Angularty',
+            isActive: true,
+            type: "Work",
+            datetime: "2021-01-05T15:27:08"
+        },
+        {
+            title: 'ReactJs',
+            isActive: false,
+            type: "Training",
+            datetime: "2021-01-05T15:27:08"
+        },
+        {
+            title: 'React Native',
+            isActive: true,
+            type: "Outside",
+            datetime: "2021-01-05T15:27:08"
+        },
+        {
+            title: 'HTML',
+            isActive: true,
+            type: "Work",
+            datetime: "2021-01-05T15:27:08"
+        },
+        {
+            title: 'NCC Soft',
+            isActive: true,
+            type: "Training",
+            datetime: "2021-01-05T15:27:08"
+        },
+        {
+            title: 'VueJS',
+            isActive: true,
+            type: "Outside",
+            datetime: "2021-01-05T15:27:08"
+        },
+    ]
 
-    useEffect(() => {
-    }, [])
+    // const [isLoaded, setIsLoaded] = useState(false);
+    const [listTodo, setListUser] = useState(todoItems);
+    const [checkAllActive, setcheckAllActive] = useState(false);
+    const [listTodoFilter, setListTodoFilter] = useState(listTodo);
+    
+    useEffect(() => { }, []);
+
+    const allActiveItem = () => {
+        listTodo.forEach(e => {
+            e.isActive = checkAllActive;
+        })
+        setcheckAllActive(!checkAllActive);
+    }
+
+    const checkBoxActive = (item:any) => {
+        let isActive = item.isActive;
+        let index = listTodo.indexOf(item);
+        setListUser(
+            [...listTodo.slice(0, index),
+            {...item, isActive: !isActive }, 
+            ...listTodo.slice(index + 1)]
+        );
+    };
 
     const test = () => {
         console.log(123);
     };
-    
-    const onSearch = () => {
 
+    const onSearch = () => { };
+
+    const delItem = (item:any) => {
+        let index = listTodo.indexOf(item);
+        setListUser(
+            [...listTodo.slice(0, index),
+            ...listTodo.slice(index + 1)]
+        );
+    };
+
+    const filterItem = (value:any) => {
+        let list = (value===1) ? listTodo : listTodo.filter(e => e.isActive === value);
+        setListTodoFilter(list);
     }
 
     return (
         <div className="todo">
             <div className="todo-title">
-                <Divider orientation="center" className="f-s-24">Total List TSX</Divider>
+                <Divider orientation="center" className="f-s-24">
+                    Total List TSX
+        </Divider>
             </div>
             <div className="todo-main">
                 <List
                     header={
-                        <div>
-                            <Button onClick={() => test()} type="primary">Theem </Button>
-                            <Search placeholder="Search text" onSearch={onSearch} enterButton style={{ width: 200 }}/>
-                            <Button onClick={() => test()} type="primary">Theem </Button>
-                        </div>}
-                    footer={<div>Footer</div>}
+                        <div className="list-header">
+                            <Button onClick={() => allActiveItem()} type="primary">
+                                <div className="check-all" onClick={() => test()}></div>
+                            </Button>
+                            <Search placeholder="Search text" onSearch={onSearch} enterButton style={{ width: 340 }} />
+                            <Button onClick={() => test()} type="primary">Thêm Mới</Button>
+                        </div>
+                    }
+                    footer={
+                        <div className="list-footer">
+                            <div className="total-active">{listTodoFilter.filter(e => e.isActive === true).length} items left</div>
+                            <Button onClick={() => filterItem(1)} type="primary">All</Button>
+                            <Button onClick={() => filterItem(false)} type="primary">Active</Button>
+                            <Button onClick={() => filterItem(true)} type="primary">Completed</Button>
+                        </div>
+                    }
                     bordered
-                    dataSource={data}
-                    renderItem={item => (
+                    dataSource={listTodoFilter}
+                    renderItem={(item) => (
                         <List.Item>
-                            <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                            <TodoItem item={item} isActiveItem={() => checkBoxActive(item)} delItem={() => delItem(item)}></TodoItem>
                         </List.Item>
                     )}
                 />
             </div>
         </div>
-    )
-}
-
+    );
+};
